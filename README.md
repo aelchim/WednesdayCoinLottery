34f8a0cf74831cbb9c664c37d6ac6b187715cf4e
aelchim <aelchim@yahoo.com>
2018-03-06 00:49:10 -0500

    Updated WednesdayCoinLottery to a complex implementation that requires a manual ranom from us

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 # WednesdayCoinLottery
-We are using the Oraclize approach for this lottery game for WednesdayCoin. In this game people will use WednesdayCoin to enter the lottery. That coin goes into the pot. The pot then continues to grow until it hits the designated jackpot amount (starts at 100,000 WED). Once the pot size is greater than or equal to the jackpot, the lottery pays out. Once it pays out the pot resets to 0 and the jackpot gets upped by 100,000 WED for next time.
+We are using a different approach for this lottery game. In this game people will use WednesdayCoin to enter the lottery. That coin goes into the pot. The pot then continues to grow until some time has passed, say 6 hours. Once enough time has passed, the lottery payout wil be initiated by the contract owner. Once it pays out the pot resets to 0.
 
 ## Approach
-This is a simplier approach where we do have a list of entries. Each entry is now 10,000 WED, that is what is currently set but can be changed later. Each entry will be added to an array an once jackpot is reached, we will randomly select one.
+This is a simpler approach where we do have a list of entries. Each entry is now 10,000 WED, that is what is currently set but can be changed later. Each entry will be added to an array and once payout is initiated, we will randomly select one.
 
 ## Random
-We are using Oraclize to for random. This is great since the lottery will be securely random, but this approach costs gas. Our original approach of just keeping track of who is in the "lead" is too expensive for this approach. We can mitigate some of this by allowing donations for ether.
-
+We are deciding random ourselves before the lottery. To prove this we are storing the hash of the random number in the contract before the lottery begins. Once the payout will occur we will pass the random number and the contract will hash it and compare to the stored hashProof. This way we can prove that the random number was decided before the lottery begins and that is was NOT changed while the lottery was occurring. This way we prove that the random number has not been influenced based on the entries.
---
+++ complex/WednesdayLotteryComplex.sol
@@ -0,0 +1,117 @@
+pragma solidity ^0.4.18;
+
+import "./ownable.sol";
+import "./destructible.sol";
+import "./tokenInterfaces.sol";
+
+/**
+ *  More complex lottery implementation
+ *  We keep track of all entries
+ *  End to lottery will be
+ */
+
+
+/**
+ * @title WednesdayCoinLottery
+ * @dev WednesdayCoinLottery is a token lottery where people will give tokens to enter
+ * and once enough time has passed the lottery will give the pot to a random person
+ */
+contract WednesdayCoinLottery is Ownable, Destructible {
+
+    // WednesdayCoin contract being held
+    WednesdayCoin public wednesdayCoin;
+
+    // list of entries
+    address[] public entries;
+
+    // current total pot size
+    uint256 public potSize;
+
+    //Hash proof of initial random value, this is set (roughly) before the lottery begins
+    uint256 public hashProof;
+
+    //combined hash of entry addresses
+    uint256 public aggregateHash = 0x0;
+
+    //10k entry
+    uint256 public contribution = 10000000000000000000000;
+
+    bool public stopLottery;
+
+    function WednesdayCoinLottery() {
+        wednesdayCoin = WednesdayCoin(0xEDFc38FEd24F14aca994C47AF95A14a46FBbAA16);
+        potSize = 0;
+        stopLottery = false;
+    }
+
+    function receiveApproval(address from, uint256 value, address tokenContract, bytes extraData) returns (bool) {
+        require(stopLottery == false);
+
+        if (wednesdayCoin.transferFrom(from, this, value)) {
+            //from check to owner allows owner to up the pot with being put as an entry
+            if (from != owner) {
+                require(value == contribution);
+                entries.push(from);
+                aggregateHash = uint256(keccak256(aggregateHash, from));
+            }
+
+            //add value to pot regardless of owner or not
+            potSize += value;
+        }
+    }
+
+    /**
+     * @notice Transfers tokens to random winner
+     */
+    function release(uint256 _randomNumber, uint256 _newHashProof) public onlyOwner {
+        require(stopLottery == false);
+        //require Wednesday(3)
+        uint8 dayOfWeek = uint8((now / 86400 + 4) % 7);
+        require(dayOfWeek == 3);
+
+        //Check hat the hashproof has been set
+        require(hashProof != 0x0);
+
+        //This is to prove that the random number we submit is the same one calculated before the start of the lottery
+        require(uint256(keccak256(_randomNumber)) == hashProof);
+
+        uint256 randomInt = uint256(keccak256(aggregateHash, _randomNumber)) % (entries.length - 1);
+        address winner = entries[randomInt];
+
+        //check that winner is not 0x0
+        require(winner != 0x0);
+
+        wednesdayCoin.transfer(winner, potSize);
+
+        potSize = 0;
+
+        //This is the new hashProof for the next lottery to prove once again that the random number is calculated before the start of the lottery
+        hashProof = _newHashProof;
+    }
+
+    function setContribution(uint256 _newContribution) public onlyOwner {
+        contribution = _newContribution;
+    }
+
+    function setStopLottery(bool _stopLottery) public onlyOwner {
+        stopLottery = _stopLottery;
+    }
+
+    // Used for transferring any accidentally sent ERC20 Token by the owner only
+    function transferAnyERC20Token(address tokenAddress, uint tokens) public onlyOwner returns (bool success) {
+        return ERC20Interface(tokenAddress).transfer(owner, tokens);
+    }
+
+    // Used for transferring any accidentally sent Ether by the owner only
+    function transferEther(address dest, uint amount) public onlyOwner {
+        dest.transfer(amount);
+    }
+
+    function setHashProof(uint256 _hashProof) public onlyOwner {
+        hashProof = _hashProof;
+    }
+
+    function clearEntries() public onlyOwner {
+        delete entries;
+    }
+}
